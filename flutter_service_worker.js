2d63e3d2e73c06af8c75aadfd800e24bbb63c00a
Nakul Dev <nakuldev1561@gmail.com>
2024-09-18 15:07:55 +0530

    update

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -152,7 +152,7 @@ const RESOURCES = {"assets/AssetManifest.bin": "69d2ec5a6e968d1633e7978f62ae6c79
 "canvaskit/skwasm.worker.js": "89990e8c92bcb123999aa81f7e203b1c",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "flutter.js": "f393d3c16b631f36852323de8e583132",
-"flutter_bootstrap.js": "13cc49bd69b1b53a14cc44e46c322365",
+"flutter_bootstrap.js": "609dacf8614577284f963463c345c2d0",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
